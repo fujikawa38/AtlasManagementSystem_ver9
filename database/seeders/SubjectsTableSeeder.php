edc2d0246aea45fe368f06ab935adf97de10394d
fujikawa38 <tacchi.juc.08lastwin@gmail.com>
2024-12-27 23:41:12 +0900

    デバッグバーの導入と初期データの登録完了

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+
 class SubjectsTableSeeder extends Seeder
 {
     /**
@@ -15,5 +17,10 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            ['subject' => '国語'],
+            ['subject' => '数学'],
+            ['subject' => '英語'],
+        ]);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -13,6 +15,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            ['over_name' => '田中',
+            'under_name' => '太郎',
+            'over_name_kana' => 'タナカ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'a@mail.com',
+            'sex' => '1',
+            'birth_day' => '2000-01-02',
+            'role' => '4',
+            'password' => bcrypt('12345678')
+            ],
+            ['over_name' => '田中',
+            'under_name' => '二子',
+            'over_name_kana' => 'タナカ',
+            'under_name_kana' => 'ニコ',
+            'mail_address' => 'b@mail.com',
+            'sex' => '2',
+            'birth_day' => '2000-01-03',
+            'role' => '1',
+            'password' => bcrypt('12345678')
+            ],
+        ]);
     }
 }
